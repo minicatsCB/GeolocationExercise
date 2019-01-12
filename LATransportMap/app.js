190a1ec1a3c99a04ba5d71816d362649d74970a4
Maribel <minicatsCB@users.noreply.github.com>
2019-01-12 23:18:51 +0100

    Put a marker at each vehicle position

---
+++ LATransportMap/app.js
@@ -0,0 +1,42 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        // Use Los Angeles coordinates for initializing the map
+        center: {
+            lat: 34.052235,
+            lng: -118.243683
+        },
+        zoom: 11
+    });
+}
+
+async function getVehicles() {
+    var vehicles = await requestData(baseVehiclesUrl);
+    return vehicles;
+}
+
+function createMarker(vehiclePosition) {
+    var marker = new google.maps.Marker({
+        position: vehiclePosition,
+        map: map
+    });
+}
+
+function positionVehicle(vehicle) {
+    var vehiclePosition = {
+        lat: vehicle.latitude,
+        lng: vehicle.longitude
+    };
+
+    createMarker(vehiclePosition);
+}
+
+async function showVehiclesInMap() {
+    var vehicles = await getVehicles();
+    vehicles.items.forEach(positionVehicle);
+}
+
+var baseVehiclesUrl = "http://api.metro.net/agencies/lametro/vehicles/";
+var map;
+
+initMap();
+showVehiclesInMap();
